f8a58f3e345d868033e56aea7efa8b8cc5b531ec
ayumii <april_angela2@hotmail.com>
2015-03-17 20:50:29 -0700

    added a simple map for the game
    using visual studio with typescript

---
+++ game/app.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="phaser.d.ts"/>
+var map;
+var layer;
+var stickmanSprite;
+
+var game = new Phaser.Game(1344, 608, Phaser.AUTO, '', { preload: this.preload, create: this.create, update: this.update });
+
+function preload() {
+    game.load.tilemap('map', 'map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('ground_1x1', 'ground_1x1.png');
+    game.load.image('stickman', 'stickman.png');
+}
+
+function create() {
+    map = game.add.tilemap('map');
+    map.addTilesetImage('ground_1x1');
+    layer = map.createLayer('Tile Layer 1');
+    layer.resizeWorld();
+}
+
+function update() {
+}
+//# sourceMappingURL=app.js.map
---
+++ game/app.ts
@@ -0,0 +1,29 @@
+﻿
+/// <reference path="phaser.d.ts"/>
+var map; // The tilemap
+var layer; // A layer within a tileset
+var stickmanSprite;
+
+var game = new Phaser.Game
+    (1344, 608, Phaser.AUTO, '',
+    { preload: this.preload, create: this.create, update: this.update });
+
+function preload() {
+    
+    game.load.tilemap('map', 'map.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('ground_1x1', 'ground_1x1.png');
+    game.load.image('stickman', 'stickman.png');
+    
+}
+
+function create() {
+   map = game.add.tilemap('map');
+   map.addTilesetImage('ground_1x1');
+   layer = map.createLayer('Tile Layer 1');
+   layer.resizeWorld();
+
+}
+
+function update() {
+ 
+}
